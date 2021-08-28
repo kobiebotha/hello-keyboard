30044264d0acf01e3b0cb81b35d3863b3a469208
Kobie Botha <kobie@journeyapps.com>
2021-08-28 12:01:18 -0600

    mp3s to support chromium
    chromium doesn't ship with any Web Speech API voices, so we use some mp3s instead.

---
+++ script.js
@@ -0,0 +1,45 @@
+var synth = new Tone.Synth().toDestination();
+
+function col() {
+  var hexValues = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+  ];
+  var newColor = '#';
+
+  for (var i = 0; i < 6; i++) {
+    var x = Math.round(Math.random() * 14);
+    var y = hexValues[x];
+    newColor += y;
+  }
+  return newColor;
+}
+
+document.addEventListener('keydown', (event) => {
+  document.body.style.backgroundColor = col();
+
+  let key = event.key;
+  document.querySelector("#keyboard-out").innerHTML = key;
+
+  //FIXME: hack. doesn't work with some characters
+  new Audio(`../audio/${key.toLowerCase()}.mp3`).play()
+
+  //random synth frequency
+  const n = `${['C', 'E', 'G'][Math.round(Math.random() * 2)]}${Math.round(Math.random() * 2) + 2
+    }`;
+  synth.volume.value = -20;
+  synth.triggerAttackRelease(n, "8n");
+});
